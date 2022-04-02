a77e85dc2fab8f988ca696d32c70053d61d08008
PutuYudana <101126261+PutuYudana@users.noreply.github.com>
2022-04-02 20:07:24 +0800

    Tugas 1 proweb

---
+++ Tugas1.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>   
+    <title>Tugas1</title>
+</head>
+<body>
+    <h1> Menghitung Luas Segitiga </h1> <br>
+    <form method="POST" action="#">
+        Nilai Alas:<br>
+        <input type="number" name="txALAS" min="0" max="999" required><br><br>
+        <form method="POST" action="#">
+            Nilai Tinggi:<br>
+                <input type="number" name="txTINGGI" min="0" max="999" required><br><br>  
+            Luas:<br>
+        <textarea name="luas" id="luas">
+      
+        <?php      
+        if(isset($_POST['hasil'])){
+                $alas    =$_POST["txALAS"];
+                $tinggi    =$_POST["txTINGGI"];
+                $luas = $alas * $tinggi * 0.5;
+                echo $luas ;
+            }
+            ?>
+              </textarea>
+            <br> <input type="submit" name="hasil" value="hasil">    
+        </form>   
+    </form>
+</body>
+</html>
